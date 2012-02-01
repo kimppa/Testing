f32968dbc487e8bc9ca5ccd938bf24bae1d9c0c9
Kim L <kim@vaadin.com>
2012-02-01 14:00:34 +0200

    Project stub + first example

---
+++ src/com/github/kimppa/examples/Calculator.java
@@ -0,0 +1,21 @@
+package com.github.kimppa.examples;
+
+public class Calculator {
+
+	/**
+	 * Divides x with y and returns the absolute value of the result of the
+	 * division.
+	 * 
+	 * @param x
+	 * @param y
+	 * @return Absolute value of x/y
+	 */
+	public float absDiv(int x, int y) {
+		if (y == 0) {
+			throw new IllegalArgumentException(
+					"Y cannot be zero. Division by zero is not allowed");
+		}
+
+		return Math.abs(((float) x) / y);
+	}
+}
---
+++ tests/com/github/kimppa/examples/CalculatorTests.java
@@ -0,0 +1,50 @@
+package com.github.kimppa.examples;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class CalculatorTests {
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void absDiv_yIsZero_exceptionThrown() {
+		new Calculator().absDiv(0, 0);
+	}
+	
+	@Test
+	public void absDiv_xIsZerp_returnsZero() {
+		float result = new Calculator().absDiv(0, 3);
+		Assert.assertEquals(0f, result);
+	}
+	
+	@Test
+	public void absDiv_xAndYCanBeDividedEvenly_returnsCorrectValue() {
+		float result = new Calculator().absDiv(12, 3);
+		Assert.assertEquals(4f, result);
+	}
+	
+	@Test
+	public void absDiv_xAndYCannotBeDividedEvenly_returnsCorrectValue() {
+		float result = new Calculator().absDiv(15, 6);
+		Assert.assertEquals(2.5f, result);
+	}
+	
+	@Test
+	public void absDiv_xIsNegative_returnsPositiveValue() {
+		float result = new Calculator().absDiv(-15, 6);
+		Assert.assertEquals(2.5f, result);
+	}
+	
+	@Test
+	public void absDiv_yIsNegative_returnsPositiveValue() {
+		float result = new Calculator().absDiv(15, -6);
+		Assert.assertEquals(2.5f, result);
+	}
+	
+	@Test
+	public void absDiv_xAndYAreNegative_returnsPositiveValue() {
+		float result = new Calculator().absDiv(-15, -6);
+		Assert.assertEquals(2.5f, result);
+	}
+
+}
